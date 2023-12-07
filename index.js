84d0d87485a3414d430009826f429cd194314208
zalish-shawon <zalish.shawon@gmail.com>
2023-12-08 03:59:06 +0600

    added api data

---
+++ index.js
@@ -0,0 +1,79 @@
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+// Close the modal if the user clicks outside of it
+window.onclick = function (event) {
+    const modal = document.getElementById('myModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const attachItems = document.querySelectorAll('.attach-files');
+    const itemsList = document.getElementById('items-list');
+    const addForm = document.getElementById('add-form');
+  
+    // Fetch data from the server and update the UI
+    const fetchData = async () => {
+        const response = await fetch('http://localhost:5000/files');
+        const data = await response.json();
+        let totalAttachData = 0;
+   
+        
+        attachItems.innerHTML = totalAttachData;
+
+        itemsList.innerHTML = data.map(item => {
+          const fileLinks = item.files.map(file => {
+            return file.originalFileName;
+          }).join('<br />');
+    
+          
+          return `<li>Uploaded Files: ${item.files.length} - ${fileLinks}</li>`;
+        }).join('');
+      
+
+      const totalFilesResponse = await fetch('http://localhost:5000/totalFiles');
+    const totalFilesData = await totalFilesResponse.json();
+
+    // Update total files information in all elements with the class "attachItems"
+    attachItems.forEach(element => {
+      element.innerText = totalFilesData.totalFiles;
+    });
+  };
+  
+    // Add a new item when the form is submitted
+    addForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+  
+      
+      const fileInputs = document.getElementById('item-files').files;
+  
+      const formData = new FormData();
+      
+  
+      for (const file of fileInputs) {
+        formData.append('files', file);
+      }
+
+  
+      await fetch('http://localhost:5000/files', {
+        method: 'POST',
+        body: formData,
+      })
+  
+  
+      fetchData();
+    });
+  
+    // Initial data fetch
+    fetchData();
+  });
+  
